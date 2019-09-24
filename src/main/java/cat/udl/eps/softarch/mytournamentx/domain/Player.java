adde243182358675c3e310fb5ecd590b7cf44fe7
bergacat1 <albertbergagatius@gmail.com>
2019-09-24 21:44:45 +0200

    Classes creation

---
+++ src/main/java/cat/udl/eps/softarch/mytournamentx/domain/Player.java
@@ -0,0 +1,16 @@
+package cat.udl.eps.softarch.mytournamentx.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class Player extends User {
+
+}
---
+++ src/main/java/cat/udl/eps/softarch/mytournamentx/domain/TournamentMaster.java
@@ -0,0 +1,13 @@
+package cat.udl.eps.softarch.mytournamentx.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class TournamentMaster extends User {
+
+}
