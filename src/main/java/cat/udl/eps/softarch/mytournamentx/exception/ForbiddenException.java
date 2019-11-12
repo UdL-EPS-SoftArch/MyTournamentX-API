a809ce8a9267271f47886e95f33c438a696ec5d0
dbm5 <parcheado2347@gmail.com>
2019-11-12 18:02:05 +0100

    Added exception and check for leader in handler

--- src/main/java/cat/udl/eps/softarch/mytournamentx/exception/ForbiddenException.java
+++ src/main/java/cat/udl/eps/softarch/mytournamentx/exception/ForbiddenException.java
@@ -5,8 +5,4 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.FORBIDDEN, reason = "Unauthorized")
 public class ForbiddenException extends RuntimeException {
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> origin/CreateMatchResult
