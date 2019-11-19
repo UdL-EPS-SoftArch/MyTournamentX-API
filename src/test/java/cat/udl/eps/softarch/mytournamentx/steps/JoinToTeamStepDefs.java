09f5c405bd2e18dabac01787887f46aaadc46446
rbague5 <rogerbm26@gmail.com>
2019-11-19 17:56:46 +0100

    Created file JoinToTeamStefDefs

---
+++ src/test/java/cat/udl/eps/softarch/mytournamentx/steps/JoinToTeamStepDefs.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.mytournamentx.steps;
+
+import cucumber.api.java.en.When;
+
+public class JoinToTeamStepDefs {
+    @When("^I try to join that team$")
+    public void iTryToJoinThatTeam() {
+    }
+}
