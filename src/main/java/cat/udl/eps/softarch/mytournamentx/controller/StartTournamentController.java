01e35e08a2a88ceee9154d15ee8fa050cae2d020
Toni Pacheco <toni.pafe@gmail.com>
2019-11-21 15:32:37 +0100

    feat: added controller to start tournaments

---
+++ src/main/java/cat/udl/eps/softarch/mytournamentx/controller/StartTournamentController.java
@@ -0,0 +1,39 @@
+package cat.udl.eps.softarch.mytournamentx.controller;
+
+import cat.udl.eps.softarch.mytournamentx.domain.Tournament;
+import cat.udl.eps.softarch.mytournamentx.domain.types.TournamentState;
+import cat.udl.eps.softarch.mytournamentx.exception.ForbiddenException;
+import cat.udl.eps.softarch.mytournamentx.service.TournamentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.BasePathAwareController;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@BasePathAwareController
+public class StartTournamentController {
+
+    @Autowired
+    TournamentService tournamentService;
+
+    @PostMapping("/tournaments/{name}/start")
+    public @ResponseBody
+    PersistentEntityResource getCreatedTournamentIdentity(
+            @PathVariable(value="name") String name,
+            PersistentEntityResourceAssembler resourceAssembler) {
+
+        Tournament tournament = tournamentService.getTournament(name);
+
+        if(tournament.getState() != TournamentState.UNINITIALIZED)
+        {
+            throw new ForbiddenException();
+        }
+
+        tournamentService.createTournament(tournament);
+
+        return resourceAssembler.toResource(tournament);
+    }
+}
