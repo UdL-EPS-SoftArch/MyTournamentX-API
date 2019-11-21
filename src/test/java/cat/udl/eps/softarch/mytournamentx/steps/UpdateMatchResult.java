ed29080bc37228945b73c373633097b094331773
Sactorum <parcheadp2347@gmail.com>
2019-11-21 15:58:59 +0100

    StepDefCreated

---
+++ src/test/java/cat/udl/eps/softarch/mytournamentx/steps/UpdateMatchResult.java
@@ -0,0 +1,9 @@
+package cat.udl.eps.softarch.mytournamentx.steps;
+
+import cucumber.api.java.en.Given;
+
+public class UpdateMatchResult {
+    @Given("^There some matches with MatchResults$")
+    public void thereSomeMatchesWithMatchResults() {
+    }
+}
