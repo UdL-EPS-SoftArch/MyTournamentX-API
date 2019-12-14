acd68b4077e23303b2fae3fecb8fb9e847dd77e8
Roberto García <roberto@rhizomik.net>
2019-12-14 19:56:26 +0100

    Remove unused imports

--- src/main/java/cat/udl/eps/softarch/mytournamentx/handler/UpdateMatchHandler.java
+++ src/main/java/cat/udl/eps/softarch/mytournamentx/handler/UpdateMatchHandler.java
@@ -1,35 +1,18 @@
 package cat.udl.eps.softarch.mytournamentx.handler;
 
-import cat.udl.eps.softarch.mytournamentx.domain.MatchResult;
-import cat.udl.eps.softarch.mytournamentx.domain.Player;
 import cat.udl.eps.softarch.mytournamentx.domain.Match;
-import cat.udl.eps.softarch.mytournamentx.domain.Team;
-import cat.udl.eps.softarch.mytournamentx.exception.ForbiddenException;
-import cat.udl.eps.softarch.mytournamentx.repository.MatchRepository;
 import cat.udl.eps.softarch.mytournamentx.service.MatchWinnerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.rest.core.annotation.HandleBeforeCreate;
-import org.springframework.data.rest.core.annotation.HandleBeforeDelete;
-import org.springframework.data.rest.core.annotation.HandleAfterSave;
-import org.springframework.data.rest.core.annotation.HandleBeforeSave;
-import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.data.rest.core.annotation.HandleAfterCreate;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.data.rest.core.annotation.RepositoryEventHandler;
 import org.springframework.stereotype.Component;
-import org.springframework.test.web.servlet.ResultActions;
-
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @RepositoryEventHandler
 public class UpdateMatchHandler {
     private final Logger logger = LoggerFactory.getLogger(Match.class);
-    protected ResultActions result;
 
     @Autowired
     MatchWinnerService matchWinnerService;
